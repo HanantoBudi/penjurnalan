a471231efda93de1ae02fc9c59e6f4ea8a2eca00
HanantoBudi <hanantobudiprasetio@gmail.com>
2022-08-13 15:22:43 +0700

    First Commit [Tio]

---
+++ src/main/java/id/co/askrindo/penjurnalan/PenjurnalanApplication.java
@@ -0,0 +1,13 @@
+package id.co.askrindo.penjurnalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenjurnalanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenjurnalanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/askrindo/penjurnalan/PenjurnalanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.askrindo.penjurnalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenjurnalanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
